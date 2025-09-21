b11999df3c2fb15360192182d48941004d488dd3
luisgs7 <luisgsilva270@gmail.com>
2025-09-20 22:11:51 -0300

    aula 461

--- lib/components/Cronometro.dart
+++ lib/components/Cronometro.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pomodoro/components/cronometroBotao.dart';
 
 class Cronometro extends StatelessWidget {
   const Cronometro({super.key});
@@ -24,7 +25,34 @@ class Cronometro extends StatelessWidget {
               fontSize: 120,
               color: Colors.white,
             ),
-          )
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: EdgeInsets.only(right: 10),
+                child: CronometroBotao(
+                  texto: 'Iniciar',
+                  icone: Icons.play_arrow,
+                ),
+              ),
+              // Padding(
+              //   padding: EdgeInsets.only(right: 10),
+              //   child: CronometroBotao(
+              //     texto: 'Pausar',
+              //     icone: Icons.stop,
+              //   ),
+              // ),
+              Padding(
+                padding: EdgeInsets.only(left: 10),
+                child: CronometroBotao(
+                  texto: 'Reiniciar',
+                  icone: Icons.refresh,
+                ),
+              ),
+            ],
+          ),
         ],
       ),
     );
---
+++ lib/components/cronometroBotao.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class CronometroBotao extends StatelessWidget {
+  final String texto;
+  final IconData icone;
+  const CronometroBotao({super.key, required this.texto, required this.icone});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.black,
+        padding: const EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 20,
+        ),
+        textStyle: const TextStyle(
+          fontSize: 25,
+        ),
+      ),
+      onPressed: () {},
+      child: Row(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(
+              icone,
+              size: 25,
+              color: Colors.white,
+            ),
+          ),
+          Text(
+            texto,
+            style: const TextStyle(fontSize: 25, color: Colors.white),
+          ),
+        ],
+      ),
+    );
+  }
+}
